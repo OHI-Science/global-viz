3c70f8eafe4b6a40efb7b2fc69ed6dab5eebcdb9
eleanorecc <eleanorecampbell@ucsb.edu>
2018-08-17 17:16:23 -0700

    region cards template!

--- region_cards/region_card_src.R
+++ region_cards/region_card_src.R
@@ -3,9 +3,13 @@ library(ggplot2)
 library(stringr)
 
 dir.create(path = "output/")
-opts_knit$set(base.dir = "output/")
+knitr::opts_knit$set(base.dir = "output/", echo = FALSE)
 
-for(i in 1:5) {
+regioncard_title <- paste("COUNTRYNAME")
+makedate <- format(Sys.time(), "%b %d %Y")
+
+for(i in 1:2) {
   filename <- str_to_lower(str_replace_all(rownames(mtcars), " ", ""))[i]
   knit2pdf(input  = "region_card.Rnw", output = paste0("output/", filename, ".tex"), compiler = "xelatex")
+  testplot <- ggplot(mtcars[i,], aes(x = cyl, y = disp) ) + geom_point()
 }
